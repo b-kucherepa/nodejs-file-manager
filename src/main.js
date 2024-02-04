0e08dd533bc3c7d76229515064735a128870d58b
b-kucherepa <bkuchr@gmail.com>
2024-02-05 00:41:38 +0400

    feat: fully implement navigation.js (add listDir method)

--- src/main.js
+++ src/main.js
@@ -11,75 +11,77 @@ const execCommand = async (command) => {
     const commandName = parseCommand(command)[0];
     const commandArgs = parseCommand(command).slice(1);
 
-    //try {
-    switch (commandName) {
-        case '.exit':
-            navigation.exit();
-            break;
-        case 'up':
-            navigation.goDirUp();
-            break;
-        case 'cd':
-            const newWdPath = buildPath(commandArgs[0]);
-            navigation.changeDir(newWdPath);
-            break;
-        case 'ls':
-            navigation.listDir();
-            break;
-        case 'cat':
-            const readPath = buildPath(commandArgs.join(' '));
-            filesystem.readFile(readPath);
-            break;
-        case 'add':
-            const createPath = buildPath(commandArgs[0]);
-            const dataToWrite = commandArgs.slice(1).join(' ');
-            filesystem.createFile(createPath, dataToWrite);
-            break;
-        case 'rn':
-            const initNamePath = buildPath(commandArgs[0]);
-            const newNamePath = buildPath(commandArgs[1]);
-            filesystem.renameFile(initNamePath, newNamePath);
-            break;
-        case 'cp':
-            const sourcePath = buildPath(commandArgs[0]);
-            const destPath = buildPath(commandArgs[1]);
-            filesystem.copyFile(sourcePath, destPath);
-            break;
-        case 'mv':
-            const initPath = buildPath(commandArgs[0]);
-            const newPath = buildPath(commandArgs[1]);
-            filesystem.moveFile(initPath, newPath);
-            break;
-        case 'rm':
-            const deletePath = buildPath(commandArgs[0]);
-            filesystem.deleteFile(deletePath);
-            break;
-        case 'os':
-            const infoType = commandArgs[0];
-            osinfo.printOsInfo(infoType);
-            break;
-        case 'hash':
-            const fileToHash = commandArgs[0];
-            cryptography.calcFileHash(fileToHash);
-            break;
-        case 'compress':
-            const fileToCompress = buildPath(commandArgs[0]);
-            const compressPath = buildPath(commandArgs[1]);
-            compress.brotliCompress(fileToCompress, compressPath);
-            break;
-        case 'decompress':
-            const compressedFile = buildPath(commandArgs[0]);
-            const decompressPath = buildPath(commandArgs[1]);
-            compress.brotliDecompress(compressedFile, decompressPath);
-            break;
-        default:
-            console.log('Invalid command');
-            break;
+    try {
+        switch (commandName) {
+            case '.exit':
+                navigation.exit();
+                break;
+            case 'up':
+                navigation.goDirUp();
+                break;
+            case 'cd':
+                const newWdPath = buildPath(commandArgs[0]);
+                navigation.changeDir(newWdPath);
+                break;
+            case 'ls':
+                navigation.listDir();
+                break;
+            case 'cat':
+                const readPath = buildPath(commandArgs.join(' '));
+                filesystem.readFile(readPath);
+                break;
+            case 'add':
+                const createPath = buildPath(commandArgs[0]);
+                const dataToWrite = commandArgs.slice(1).join(' ');
+                filesystem.createFile(createPath, dataToWrite);
+                break;
+            case 'rn':
+                const initNamePath = buildPath(commandArgs[0]);
+                const newNamePath = buildPath(commandArgs[1]);
+                filesystem.renameFile(initNamePath, newNamePath);
+                break;
+            case 'cp':
+                const sourcePath = buildPath(commandArgs[0]);
+                const destPath = buildPath(commandArgs[1]);
+                filesystem.copyFile(sourcePath, destPath);
+                break;
+            case 'mv':
+                const initPath = buildPath(commandArgs[0]);
+                const newPath = buildPath(commandArgs[1]);
+                filesystem.moveFile(initPath, newPath);
+                break;
+            case 'rm':
+                const deletePath = buildPath(commandArgs[0]);
+                filesystem.deleteFile(deletePath);
+                break;
+            case 'os':
+                const infoType = commandArgs[0];
+                osinfo.printOsInfo(infoType);
+                break;
+            case 'hash':
+                const fileToHash = commandArgs[0];
+                cryptography.calcFileHash(fileToHash);
+                break;
+            case 'compress':
+                const fileToCompress = buildPath(commandArgs[0]);
+                const compressPath = buildPath(commandArgs[1]);
+                compress.brotliCompress(fileToCompress, compressPath);
+                break;
+            case 'decompress':
+                const compressedFile = buildPath(commandArgs[0]);
+                const decompressPath = buildPath(commandArgs[1]);
+                compress.brotliDecompress(compressedFile, decompressPath);
+                break;
+            default:
+                console.log('Invalid command');
+                break;
+        }
     }
-    //}
-    //catch {
-    //console.log('Operation failed');
-    //}
+    catch {
+        console.log('Operation failed');
+    }
+    
+    navigation.printCwd();
 }
 
 const parseCommand = (command) => command.toString().replace(EOL, '').split(' ');
--- src/modules/navigation.js
+++ src/modules/navigation.js
@@ -1,31 +1,34 @@
-import {readdir} from 'fs/promises';
+import { readdir } from 'fs/promises';
 import { homedir } from 'os';
 
 export const exit = (args) => process.exit();
 
-export const goDirUp = () => {
-    process.chdir('..');
-    printCwd();
-}
+export const goDirUp = () => process.chdir('..');
 
-export const changeDir = (newPath) => {
-    process.chdir(newPath);
-    printCwd();
-}
+export const changeDir = (newPath) => process.chdir(newPath);
 
 export const setCwdToHomedir = () => process.chdir(homedir());
 
 export const printCwd = () => console.log(`< You are currently in ${process.cwd()}`);
 
 export const listDir = async () => {
-    const files = await readdir(process.cwd(), {withFileTypes: true});
+    const files = await readdir(process.cwd(), { withFileTypes: true });
     const sortedFiles = sortFiles(files);
     console.table(sortedFiles);
 }
 
-const sortFiles= (files) => {
-    const sortedFiles = files.sort((a, b) => a.isFile() - b.isFile()).filter((item) => !item.isSymbolicLink);
-    console.log(files);
-    console.log(sortedFiles);
-    //const filesTable = 
+const sortFiles = (files) => {
+    let dirArray = [];
+    let fileArray = [];
+
+    files.forEach((element) => {
+        if (element.isDirectory()) {
+            dirArray.push({ Name: element.name, Type: 'directory' });
+        }
+        else {
+            fileArray.push({ Name: element.name, Type: 'file' });
+        }
+    });
+    
+    return dirArray.concat(fileArray);
 }
